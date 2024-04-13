39b112e877d9f2069fe96d4c571f677538c6dbbd
maotovisk <me@maot.dev>
2024-04-13 13:23:38 -0300

    feat(ui): implement more hitsound copier ui

--- MapWizard.Desktop/Views/HitsoundCopier/HitsoundCopierView.axaml.cs
+++ MapWizard.Desktop/Views/HitsoundCopier/HitsoundCopierView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using MapWizard.Desktop.ViewModels;
 
 namespace MapWizard.Desktop.Views;
 
@@ -9,5 +10,6 @@ public partial class HitsoundCopierView : UserControl
     public HitsoundCopierView()
     {
         InitializeComponent();
+        DataContext = new HitsoundCopierViewModel();
     }
 }
