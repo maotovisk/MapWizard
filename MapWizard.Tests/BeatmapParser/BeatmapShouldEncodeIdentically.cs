6135231e078c9347378ed4821c2ffc464cfc3131
maotovisk <me@maot.dev>
2024-04-13 19:57:03 -0300

    tests(parser): add encoding test

---
+++ MapWizard.Tests/BeatmapParser/BeatmapShouldEncodeIdentically.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using MapWizard.BeatmapParser;
+
+namespace MapWizard.Tests.BeatmapParser;
+
+public class BeatmapShouldEncodeIdentically
+{
+    [Fact]
+    public void Encode_InputIsBeatmapString_ReturnsTrue()
+    {
+        // Arrange
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetManifestResourceStream("MapWizard.Tests.Resources.test5.osu");
+        using var reader = new StreamReader(stream);
+        var beatmapString = reader.ReadToEnd();
+        
+        // Act
+        var encodedBeatmap = Beatmap.Decode(beatmapString).Encode();
+        
+        // Assert
+        Assert.Equal(beatmapString, encodedBeatmap);
+    }
+}
