53e7bc99fd4cda177249474c6e591a8ecd1342b2
maotovisk <me@maot.dev>
2024-09-18 21:12:44 -0300

    refactor: transform filepicker into templatedcontrol

--- MapWizard.Desktop/ViewModels/HitsoundCopierViewModel.cs
+++ MapWizard.Desktop/ViewModels/HitsoundCopierViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,31 +15,62 @@ namespace MapWizard.Desktop.ViewModels;
 
 public partial class HitsoundCopierViewModel : ViewModelBase
 {
-    public string Message { get; set; }
+    public string Message { get; set; } = "Hitsound Copier View";
 
-    [ObservableProperty]
-    private AvaloniaList<SelectedMap> _originBeatmapPath = [];
+    private AvaloniaList<SelectedMap> _originBeatmapPath;
 
-    [ObservableProperty]
     private AvaloniaList<SelectedMap> _destinationBeatmapPath = [];
+    
+    public AvaloniaList<SelectedMap> OriginBeatmapPath
+    {
+        get => _originBeatmapPath;
+        set
+        {
+            if (_originBeatmapPath != value)
+            {
+                _originBeatmapPath = value;
+                OnPropertyChanged(nameof(OriginBeatmapPath));
+            }
+        }
+    }
+
+    public AvaloniaList<SelectedMap> DestinationBeatmapPath
+    {
+        get => _destinationBeatmapPath;
+        set
+        {
+            if (_destinationBeatmapPath != value)
+            {
+                _destinationBeatmapPath = value;
+                OnPropertyChanged(nameof(DestinationBeatmapPath));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
     
     [RelayCommand]
     void CopyHitsounds()
     {
         Console.WriteLine("Selected origin beatmaps: ");
+        
+        // for some reason OriginBeatmapPath is not being backpopulated from the BeatmapFileSelector user control
+        
+        
         foreach(var bm in OriginBeatmapPath)
         {
             Console.WriteLine(bm.Path);
         }
+        
         Console.WriteLine("Selected destination beatmaps: ");
         foreach(var bm in DestinationBeatmapPath)
         {
             Console.WriteLine(bm.Path);
         }
-    }
-
-    public HitsoundCopierViewModel()
-    {
-        Message = "Hitsound Copier View";
     }
 }
