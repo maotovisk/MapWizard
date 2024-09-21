43ed8091cdd93359bfba4826ea21989b332fcc85
maotovisk <me@maot.dev>
2024-09-20 22:56:50 -0300

    add(desktop): velopack bundling

--- MapWizard.Desktop/Program.cs
+++ MapWizard.Desktop/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Avalonia;
 using System;
+using System.Threading.Tasks;
+using Velopack;
+using Velopack.Sources;
 
 namespace MapWizard.Desktop;
 
@@ -9,8 +12,13 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        VelopackApp.Build().Run();
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
@@ -18,4 +26,20 @@ sealed class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+    
+    private static async Task UpdateMyApp()
+    {
+        var mgr = new UpdateManager(new GithubSource("https://github.com/maotovisk/MapWizard", null, false, null));
+
+        // check for new version
+        var newVersion = await mgr.CheckForUpdatesAsync();
+        if (newVersion == null)
+            return; // no update available
+
+        // download new version
+        await mgr.DownloadUpdatesAsync(newVersion);
+
+        // install new version and restart app
+        mgr.ApplyUpdatesAndRestart(newVersion);
+    }
 }
