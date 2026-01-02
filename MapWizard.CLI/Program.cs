40ad18e4cbe6517c99c0c71456ed2379aa7f98ed
maotovisk <me@maot.dev>
2026-01-02 18:15:25 -0300

    feat(hitsound): enhance MapManiaSoundsToSamples with index tracking and add ConvertToSampleName method

--- MapWizard.CLI/Program.cs
+++ MapWizard.CLI/Program.cs
@@ -1,7 +1,9 @@
+using BeatmapParser;
+using BeatmapParser.Enums;
 using MapWizard.CLI.ConsoleUtils;
 using MapWizard.CLI.Services;
-using MapWizard.Services;
 using MapWizard.Tools.HitSounds.Copier;
+using MapWizard.Tools.HitSounds.Extensions;
 
 namespace MapWizard.CLI;
 
@@ -9,29 +11,69 @@ static class Program
 {
     static void Main(string[] args)
     {
-        // Create a new instance of the HitSoundService
-        var hitSoundService = new HitSoundService();
+        var mode = args.GetArgumentValue("--mode") ?? args.GetArgumentValue("-m");
         
-        // Check if the user has provided the source and target paths
-        var sourcePathProvided = IoParser.ArgumentExists(args, "--source") || IoParser.ArgumentExists(args, "-s");
-        var targetPathProvided = IoParser.ArgumentExists(args, "--target") || IoParser.ArgumentExists(args, "-t");
+        if (mode == null)
+        {
+            Console.WriteLine("Please provide a mode using the --mode or -m argument.");
+            return;
+        }
 
-        if (!sourcePathProvided)
+        switch (mode)
         {
-            Console.WriteLine("Please provide a source path using the --source or -s argument.");
-            return;
+            case "copy":
+                var sourcePath = args.GetArgumentValue("--source") ?? args.GetArgumentValue("-s");
+                var targetPath = args.GetArgumentValue("--target") ?? args.GetArgumentValue("-t");
+
+                if (sourcePath == null)
+                {
+                    Console.WriteLine("Please provide a source path using the --source or -s argument.");
+                    return;
+                }
+    
+                if (targetPath == null)
+                {
+                    Console.WriteLine("Please provide a target path using the --target or -t argument.");
+                    return;
+                }
+            
+                HsCopy(sourcePath, targetPath);
+                break;
+            case "info":
+            {
+                var path = args.GetArgumentValue("--path") ?? args.GetArgumentValue("-p");
+            
+                if (path == null)
+                {
+                    Console.WriteLine("Please provide a path using the --path or -p argument.");
+                    return;
+                }
+            
+                MapInfo(path);
+                break;
+            }
         }
         
-        if (!targetPathProvided)
+    }
+
+    private static void MapInfo(string path)
+    {
+        var sourceBeatmap = Beatmap.Decode(File.ReadAllText(path));
+
+        if (sourceBeatmap.GeneralSection.Mode == Ruleset.Mania)
         {
-            Console.WriteLine("Please provide a target path using the --target or -t argument.");
-            return;
+            var maniaMappedSamples = sourceBeatmap.MapManiaSoundsToSamples();
+            
+            foreach (var (fileName, sampleData ) in maniaMappedSamples)
+            {
+                Console.WriteLine($"Mapped file '{fileName}' to SampleSet '{sampleData.sampleSet}' and HitSound '{sampleData.sound}'. with name '{sampleData.ConvertToSampleName()}'");
+            }
         }
-        
-        // Copy the hitsounds from the source path to the target path
-        
-        var sourcePath = IoParser.GetArgumentValue(args, "--source") ?? IoParser.GetArgumentValue(args, "-s") ?? throw new Exception("Source path not found.");
-        var targetPath = IoParser.GetArgumentValue(args, "--target") ?? IoParser.GetArgumentValue(args, "-t") ?? throw new Exception("Target path not found.");
+    }
+
+    private static void HsCopy(string sourcePath, string targetPath)
+    {
+        var hitSoundService = new HitSoundService();
         
         hitSoundService.CopyHitsounds(sourcePath, targetPath, new HitSoundCopierOptions());
         
--- MapWizard.Tools/HitSounds/Extensions/BeatmapExtensions.cs
+++ MapWizard.Tools/HitSounds/Extensions/BeatmapExtensions.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using BeatmapParser;
 using BeatmapParser.Enums;
 
@@ -10,16 +11,31 @@ public static class BeatmapExtensions
     /// </summary>
     /// <param name="origin">the origin beatmap.</param>
     /// <returns></returns>
-    public static Dictionary<string, (SampleSet sampleset, HitSound sound)> MapManiaSoundsToSamples(this Beatmap origin)
+    public static Dictionary<string, (SampleSet sampleSet, HitSound sound, int index)> MapManiaSoundsToSamples(this Beatmap origin)
     {
-        var sampleLookup = new Dictionary<string, (SampleSet sampleset, HitSound sound)>();
+        var sampleLookup = new Dictionary<string, (SampleSet sampleset, HitSound sound, int index)>();
+        var indexCounter = new Dictionary<(SampleSet, HitSound), int>();
 
         foreach (var hitObject in origin.HitObjects.Objects)
         {
+            var fileName = hitObject.HitSounds.SampleData.FileName;
+    
+            if (string.IsNullOrEmpty(fileName) || sampleLookup.ContainsKey(fileName))
+                continue;
             
+            var sampleSet = hitObject.HitSounds.SampleData.NormalSet;
+            
+            // ps: we are assuming the mania hint is coming from the first defined hitsound only,
+            // for now this is the only workflow supported.
+            var sound = hitObject.HitSounds.Sounds[0];
+            var key = (sampleSet, sound);
+
+            indexCounter.TryGetValue(key, out var currentIndex);
+            indexCounter[key] = ++currentIndex;
+            
+            sampleLookup.Add(fileName, (sampleSet, sound, currentIndex));
         }
         
         return sampleLookup;
-    } 
-    
+    }
 }
