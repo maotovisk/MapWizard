2e31d2b22e4c28716692fa101a2f6b6aa84d90bd
Lucas Veiga <me@maot.dev>
2026-02-19 15:15:50 -0300

    fix: remove class constraint from result

--- MapWizard.Desktop/Models/Result.cs
+++ MapWizard.Desktop/Models/Result.cs
@@ -1,6 +1,6 @@
 namespace MapWizard.Desktop.Models;
 
-public class Result<T> where T : class?
+public class Result<T>
 {
     public T? Value { get; set; }
     public ResultStatus Status { get; set; }
