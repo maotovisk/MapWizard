accc8fa0aadc43ee1fa589901fdcc5711df7af07
Lucas Veiga <me@maot.dev>
2026-02-19 14:43:45 -0300

    feat: implement system theme detection

--- MapWizard.Desktop/Services/IThemeService.cs
+++ MapWizard.Desktop/Services/IThemeService.cs
@@ -1,11 +1,15 @@
 using System;
+using MapWizard.Desktop.Models.Settings;
 
 namespace MapWizard.Desktop.Services;
 
 public interface IThemeService
 {
+    ThemeMode ThemeMode { get; }
     bool IsDarkTheme { get; }
     event EventHandler<bool>? DarkThemeChanged;
+    event EventHandler<ThemeMode>? ThemeModeChanged;
     void Initialize();
+    void SetThemeMode(ThemeMode themeMode);
     void SetDarkTheme(bool isDarkTheme);
 }
