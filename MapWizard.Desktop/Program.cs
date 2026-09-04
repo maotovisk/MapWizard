0c504d1104c4f0c594f3b4080bbc9d79ce1850d5
maotovisk <me@maot.dev>
2026-09-04 02:40:34 -0300

    feat: fix initialization for x11 and update deps

--- MapWizard.Desktop/Program.cs
+++ MapWizard.Desktop/Program.cs
@@ -35,8 +35,11 @@ internal static class Program
             .LogToTrace();
 
         if (OperatingSystem.IsLinux())
-            app.UseWayland()
-                .With(new X11PlatformOptions()
+        {
+            if (Environment.GetEnvironmentVariable("WAYLAND_DISPLAY") != null)
+                app.UseWayland();
+            
+            app.With(new X11PlatformOptions
                 {
                     RenderingMode = forceSoftwareRendering
                         ? [X11RenderingMode.Software]
@@ -47,6 +50,7 @@ internal static class Program
                             X11RenderingMode.Software
                         ]
                 });
+        }
 
         return app;
     }
